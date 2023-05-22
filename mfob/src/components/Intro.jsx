9c0ca2a21e04be7414d42dae43163ed3f5a4c1f7
xMaedayx <120540407+xMaedayx@users.noreply.github.com>
2023-05-21 19:46:17 -0700

    stash

--- mfob/src/components/Intro.jsx
+++ mfob/src/components/Intro.jsx
@@ -1,14 +1,123 @@
-import React from 'react';
-import styled from 'styled-components';
+import styled from "styled-components";
+import Navbar from "./Navbar";
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useMemo } from "react";
+import anImage from "../images/hero.png";
+import { OrbitControls, useLoader } from "@react-three/fiber";
+import {
+  AmbientLight,
+  DirectionalLight,
+  MeshDistortMaterial,
+  Sphere,
+} from "@react-three/drei";
 
 const Article = styled.article`
-height: 100vh;
-background-color: #C3B1E1; `;
+  height: 100vh;
+  background-color: #c3b1e1;
+`;
+
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1000px;
+  display: flex;
+  justify-content: space-between;
+`;
+const Top = styled.div`
+  flex: 3;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 25px;
+`;
+const Title = styled.div`
+  font-size: 50px;
+`;
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`;
+const Text = styled.div`
+  height: 5px;
+  color: #fcacbf;
+`;
+const Description = styled.div`
+  font-size: 20px;
+  color: #b0808c;
+`;
+const Button = styled.div`
+  background-color: #9caf88;
+  color: #ffffff;
+  font-weight: 500;
+  width: 150px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+const Bottom = styled.div`
+  flex: 5;
+  position: relative;
+`;
+const Img = styled.img`
+  ${(props) => (props.$primary ? "#BF4F74" : "white")};
+  color: ${(props) => (props.$primary ? "white" : "#BF4F74")};
+  width: 500px;
+  height: 500px;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #bf4f74;
+  border-radius: 3px;
+
+  animation: animate 2s infinite ease alternate;
+  @keyframes animate {
+    to {
+      transform: translateY(30px);
+    }
+  }
+`;
 
 const Intro = () => {
-    return (
-        <Article>
-            Meghan Farris O'Brien 
-        </Article>
-    ) };
-    export default Intro;
+  return (
+    <Article>
+      <Navbar />
+      <Section>
+        <Top>
+          <Title>Quoi de beau? Welcome!</Title>
+          <Content>
+            <Text>
+              I'm Meghan Farris O'Brien, a full-stack web developer based in the
+              United States.
+            </Text>
+          </Content>
+          <Description>
+            Back-end
+          </Description>
+          <Button> Learn more about me</Button>
+        </Top>
+        <Bottom>
+          <Canvas>
+            <Suspense fallback={null}>
+              <OrbitControls enableZoom={false} />
+              <AmbientLight intensity={1} />
+              <DirectionalLight position={[3, 2, 1]} />
+              <Sphere args={[1, 100, 200]} scale={2.4}>
+                <MeshDistortMaterial
+                  color="#3d1c56"
+                  attach="material"
+                  distort={0.5}
+                  speed={2}
+                />
+              </Sphere>
+            </Suspense>
+          </Canvas>
+
+          <Img src={anImage} />
+        </Bottom>
+      </Section>
+    </Article>
+  );
+};
+export default Intro;
---
+++ mfob/src/components/Navbar.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import styled from 'styled-components';
+import myImage from '../images/MFOB.png';
+import myIcon from '../images/icon.png';
+
+const Section = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  width: 1000px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0;
+`;
+
+const Links = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 45px;
+`;
+
+const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 25px;
+`;
+
+const Icon = styled.img`
+  width: 20px;
+  cursor: pointer;
+  
+`;
+
+const Logo = styled.img`
+  height: 50px;
+ 
+`;
+
+const List = styled.ul`
+  display: flex;
+  gap: 25px;
+  list-style: none;
+`;
+
+const ListItem = styled.li`
+  cursor: pointer;
+`;
+
+const Button = styled.button`
+  width: 100px;
+  padding: 10px 0;
+  background-color: #E8DEFC;
+  color: #ffffff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Navbar = () => {
+  return (
+    <Section>
+      <Container>
+        <Links>
+          <Logo src={myImage} alt="Logo" /> 
+          <List>
+            <ListItem>Home</ListItem>
+            <ListItem>About</ListItem>
+            <ListItem>Portfolio</ListItem>
+            <ListItem>Contact</ListItem>
+            <ListItem>Resume</ListItem>
+          </List>
+        </Links>
+        <Icons>
+          <Icon src={myIcon} alt="Icon" /> {/* Added alt attribute */}
+          <Button>Hire Me!</Button>
+        </Icons>
+      </Container>
+    </Section>
+  );
+}
+
+export default Navbar;
